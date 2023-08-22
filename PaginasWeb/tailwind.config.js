f80645266791774f21763e1bec182e2cfd0eabad
DanielHodrick <daniel1hodrick@gmail.com>
2023-08-22 06:22:40 -0900

    implementacion de header

---
+++ PaginasWeb/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge:[
+    './public/**/*.html',
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants:{
+    extends:{},
+  },
+  plugins: [],
+}
+
