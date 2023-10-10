3ff9110ccfea13294da2fdb427aa9076b3b31dd7
Milton2332 <daniel1hodrick@gmail.com>
2023-10-10 16:52:51 -0400

    iniciacion de VUE

---
+++ VUE/autoventa/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <!--CONCATENACION DE CANDENAS-->
+  <h1>{{ prod.toUpperCase() + " otro texto" }}</h1>
+  <!--USO DE IF TERNARIO-->
+  <h1>{{ esValido ? "valido" : "no Valido"
+  }}</h1>
+  <!--APLICACION DE COLOR A UN H1-->
+  <h1 :style="color">{{ prod.toUpperCase() + " otro texto" }}</h1>
+  <h1 v-bind:style="color2">{{ prod.toUpperCase() + " otro texto" }}</h1>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+const prod = "Automovil"
+//validacion de parametro booleano esvalido
+const esValido = true;
+const color = "color: red";
+const color2 = "color: blue"; 
+</script>
+
+<style scoped></style>
