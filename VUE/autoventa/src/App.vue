e1dd70f5e0435bfcbf4c099d69c7f7bcab82a7e6
Milton2332 <daniel1hodrick@gmail.com>
2023-10-10 17:42:24 -0400

    diseño de pagina

--- VUE/autoventa/src/App.vue
+++ VUE/autoventa/src/App.vue
@@ -1,23 +1,39 @@
 <template>
-  <!--CONCATENACION DE CANDENAS-->
-  <h1>{{ prod.toUpperCase() + " otro texto" }}</h1>
-  <!--USO DE IF TERNARIO-->
-  <h1>{{ esValido ? "valido" : "no Valido"
-  }}</h1>
-  <!--APLICACION DE COLOR A UN H1-->
-  <h1 :style="color">{{ prod.toUpperCase() + " otro texto" }}</h1>
-  <h1 v-bind:style="color2">{{ prod.toUpperCase() + " otro texto" }}</h1>
-  <div>
+  <div class="nav-bar"></div>
+  <article class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img :src="img" alt="NISSAN JUKE">
+      </div>
+      <div class="product-info">
+        <h1>{{ prod }} </h1>
+        <!--if-->
+        <!--<p v-if="disponible">Disponible</p>
+        <p v-else="">Agotado</p>-->
+        <p v-if="disponible!=0" style="color: red;">Disponible </p>
+        <p v-else="">Agotado</p>
+        <p v-if="stock>10">Variedad de colores y equipamientos</p>
+        <p v-else-if="stock<=10 && stock>0">Pocas unidades adquiera el suyo</p>
+        <p v-else>Agotado</p>
+        <ul>
+          <li v-for="d in detalles"> {{ d }}</li>
+        </ul>
+      </div>
 
-  </div>
+    </div>
+  </article>
 </template>
 
 <script setup>
+const img = './src/assets/images/blanco.png'
 const prod = "Automovil"
-//validacion de parametro booleano esvalido
-const esValido = true;
-const color = "color: red";
-const color2 = "color: blue"; 
+//--vondicionante v-if
+const disponible = true;
+
+//decalracion de stock
+const stock=5;
+//construccion de detalles
+ const detalles = ["AT","2300 cc","VXR45","OT","113 Nm"]
 </script>
 
 <style scoped></style>
